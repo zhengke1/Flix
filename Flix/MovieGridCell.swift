8b70060c187e6cb7a39261338bebf6e95f305b26
Kevin <kevinjzheng@gmail.com>
2020-10-05 16:47:19 -0400

    Details and Grid View

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kevin Zheng on 10/4/20.
+//  Copyright © 2020 kevinjzheng. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
